fdcba88c77c64f9081818cd5c41a10876c8b8c33
Gregory Romero Ramírez <gregoryrr50@hotmail.com>
2025-12-27 21:03:04 -0600

    Update requested by Andres Bonilla Made

--- Modulo2_Bases_de_Datos_Ejercicios_de_Normalizacion/-- SQLite.sql
+++ Modulo2_Bases_de_Datos_Ejercicios_de_Normalizacion/-- SQLite.sql
@@ -3,13 +3,21 @@
 -- CREATE TABLE Customer (
 --     Customer_id INT PRIMARY KEY,
 --     Customer_name VARCHAR(25) NOT NULL,
---     Customer_phone VARCHAR(20) NOT NULL,
---     Address VARCHAR(15)
+--     Customer_phone VARCHAR(20) NOT NULL
 -- );
+
+
+-- CREATE TABLE Address (
+--     Address_id INT PRIMARY KEY,
+--     Customer_id INT REFERENCES Customer(Customer_id),
+--     Address VARCHAR(25)
+-- );
+
 
 -- CREATE TABLE Orders (
 --     Order_id VARCHAR(3) PRIMARY KEY,
 --     Customer_id INT REFERENCES Customer(Customer_id),
+--     Address_id VARCHAR(25) REFERENCES Address(Address_id),
 --     Delivery_item TEXT
 -- );
 
@@ -27,18 +35,34 @@
 --     PRIMARY KEY (Order_id, Item_id)
 -- );
 
--- INSERT INTO CUSTOMER (Customer_id, Customer_name, Customer_phone, Address)
+-- INSERT INTO Customer (Customer_id, Customer_name, Customer_phone)
 --     VALUES 
---         (1, 'Alice', 123-456-7890, '123 Main Str'),
---         (2, 'Bob', 987-654-3210, '456 Elm Str'),
---         (3, 'Claire', 555-123-4567, '789 Oak Str');
+--         (1, 'Alice', '123-456-7890'),
+--         (2, 'Bob', '987-654-3210'),
+--         (3, 'Claire', '555-123-4567');
 
--- INSERT INTO Customer (Customer_id, Customer_name, Customer_phone, Address)
+
+-- INSERT INTO Address (Address_id, Customer_id, Address)
 --     VALUES
---         (1, 'Alice', '123-456-7890', '123 Main Str'),
---         (2, 'Bob', '987-654-3210', '456 Elm Str'),
---         (3, 'Claire', '555-123-4567', '789 Oak Str');
+--         (1, 1, '123 Main Str'),
+--         (2, 2, '456 Elm Str'),
+--         (3, 3, '789 Oak Str'),
+--         (4, 3, '464 Georgia St');
 
 
+-- INSERT INTO Orders (Order_id, Customer_id, Address_id, Delivery_item)
+--     VALUES
+--         ('001', 1, 1, '6:00 PM'),
+--         ('002', 2, 2, '7:30 PM'),
+--         ('003', 3, 3, '12:00 PM'),
+--         ('004', 3, 4, '5:00 PM');
 
 
+-- INSERT INTO Order_details (Order_id, Item_id, Quantity, Special_request)
+--     VALUES
+--         ('001', 101, 2, 'No Onions'),
+--         ('001', 102, 1, 'Extra Ketchup'),
+--         ('002', 103, 1, 'Extra Cheese'),
+--         ('002', 104, 2, 'None'),
+--         ('003', 105, 1, 'No Crutons'),
+--         ('004', 106, 1, 'None');
--- Modulo2_Bases_de_Datos_Ejercicios_de_Normalizacion/-- SQLiteCars.sql
+++ Modulo2_Bases_de_Datos_Ejercicios_de_Normalizacion/-- SQLiteCars.sql
@@ -6,21 +6,45 @@
 --     Owner_phone VARCHAR(20)
 -- );
 
--- CREATE TABLE Insurance (
+-- CREATE TABLE Insurance_company (
 --     Insurance_id INT PRIMARY KEY,
---     Insurance_company VARCHAR(25),
+--     Insurance_company VARCHAR(25)
+-- );
+
+-- CREATE TABLE Insurance_policy (
+--     Policy_id INT PRIMARY KEY,
+--     Insurance_id INT REFERENCES Insurance_company(Insurance_id),
 --     Insurance_policy VARCHAR(8)
 -- );
 
--- CREATE TABLE Cars (
---     Cars_id INT PRIMARY KEY,
---     VIN VARCHAR(15),
+-- CREATE TABLE Car_model (
+--     Model_id INT PRIMARY KEY,
 --     Make VARCHAR(20),
 --     Model VARCHAR(20),
---     Year INT,
+--     Year INT
+-- );
+
+-- CREATE TABLE Car_unit (
+--     Car_id INT PRIMARY KEY,
+--     VIN VARCHAR(15),
+--     Model_id INT REFERENCES Car_model(Model_id),
 --     Color VARCHAR(25)
 -- );
 
+-- INSERT INTO Insurance_company(Insurance_id, Insurance_company)
+--     VALUES
+--         (1, 'ABC Insurance'),
+--         (2, 'XYZ Insurance'),
+--         (3, 'DEF Insurance'),
+--         (4, 'GHI Insurance');
+
+
+-- INSERT INTO Insurance_policy(Policy_id, Insurance_id, Insurance_policy)
+--     VALUES
+--         (1, 1, 'POL12345'),
+--         (2, 2, 'POL54321'),
+--         (3, 3, 'POL67890'),
+--         (4, 4, 'POL98765');
 
 -- INSERT INTO Cars_owners(Cars_id, Year, Color, Owner_id, Insurance_id)
 --     VALUES
@@ -30,11 +54,17 @@
 --         (3, '2015', 'Red', 104, 111);
 
 
--- INSERT INTO Cars(Cars_id, VIN, Make, Model, Year, Color)
+-- INSERT INTO Car_model(Model_id, Make, Model, Year)
 --     VALUES
---         (1, 'IHGCM82633A', 'Honda', 'Accord', 2003, 'Silver'),
---         (2, '5J6RM4H79EL', 'Honda', 'CR-V', 2014, 'Blue'),
---         (3, '1G1RA6EH1FU', 'Chevrolet', 'Volt', 2015, 'Red');
+--         (1, 'Honda', 'Accord', 2003),
+--         (2, 'Honda', 'CR-V', 2014),
+--         (3, 'Chevrolet', 'Volt', 2015);
+
+-- INSERT INTO Car_unit(Car_id, VIN, Model_id, Color)
+--     VALUES
+--         (1, 'IHGCM82633A', 1, 'Silver'),
+--         (2, '5J6RM4H79EL', 2, 'Blue'),
+--         (3, '1G1RA6EH1FU', 3, 'Red');
 
 -- CREATE TABLE Car_Owner (
 --     car_id INT,
@@ -50,3 +80,5 @@
 --         (1, 102, 2),
 --         (2, 103, 3),
 --         (3, 104, 4);
+
+
